1d4200486c0bcb87395ba967d2c4140bd0737296
Magdalena Kołtuniak <m.koltuniak@gmail.com>
2026-02-17 19:45:01 +0100

    add task 1

--- 01/assets/js/app.js
+++ 01/assets/js/app.js
@@ -2,3 +2,27 @@ const a = '4.2';
 const b = 9;
 
 console.log(a, b);
+
+console.log(typeof a);
+console.log(typeof b);
+
+const addition = Number(a) + b;
+const concatenation = a + b.toString();
+const subtraction = a - b;
+const multiplication = a * b;
+const division = b / a;
+const modulo = b % a;
+
+console.log(
+	`addition: ${addition}, concatenation: ${concatenation}, subtraction: ${subtraction}, multiplication: ${multiplication}, division: ${division}, modulo: ${modulo}`,
+);
+
+const arr = [addition, concatenation, subtraction, multiplication, division, modulo];
+
+arr.forEach(el => {
+	if (Number(el) >= 20) {
+		console.log(`${el} jest większe od 20`);
+	} else {
+		console.log(`${el} jest mniejsze od 20`);
+	}
+});
